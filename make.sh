d78ccbd16d572bcdba650fe170c0a8b958a7cbd9
Gerrit Ansmann <bla@example.com>
2017-09-09 19:13:22 +0200

    Build script

---
+++ make.sh
@@ -0,0 +1,8 @@
+wget -nc https://i.imgur.com/1SHkwGY.png -O meme.png
+xelatex -interaction=nonstopmode -shell-escape -8bit Vortrag.tex
+xelatex -interaction=nonstopmode -shell-escape -8bit quickies.tex
+xelatex -interaction=nonstopmode -shell-escape -8bit Adventskranz.tex
+xelatex -interaction=nonstopmode -shell-escape -8bit Wach_werden.tex
+xelatex -interaction=nonstopmode -shell-escape -8bit Wochentag.tex
+xelatex -interaction=nonstopmode -shell-escape -8bit Extrema.tex
+
